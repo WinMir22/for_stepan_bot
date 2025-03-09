6b344f6d6eadeed57ff1645a094b17c14bd7e3fa
Miron <miron.kiselev11@mail.ru>
2025-03-10 01:38:14 +0300

    Create Middleware

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from config.config import Config, load_config
+from config.config import Config, get_url, load_config
 from handlers import other_handlers
+from middlewares import DBSessionMiddleware
 
 
 async def main() -> None:
@@ -15,6 +17,9 @@ async def main() -> None:
         "[%(asctime)s] - %(name)s - %(message)s",
     )
     config: Config = load_config()
+
+    engine = create_async_engine(get_url())
+    sessionmaker = async_sessionmaker(engine)
     bot = Bot(
         token=config.tg_bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
@@ -23,7 +28,7 @@ async def main() -> None:
     # dp.include_router(user_handlers.router)
     # dp.include_router(admin_handlers.router)
     dp.include_router(other_handlers.router)
-    # dp.update.outer_middleware(DBMiddleware())
+    dp.update.outer_middleware(DBSessionMiddleware(sessionmaker))
     await dp.start_polling(bot)
 
 
