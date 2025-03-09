86154a6c022669f4ee9e8c89c8b26cdab0d97dc9
Miron <miron.kiselev11@mail.ru>
2025-03-09 23:44:35 +0300

    Update command start

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,7 +1,10 @@
+import html
 import logging
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+from lexicon.lexicon_ru import lexicon
 
 router = Router()
 logger = logging.getLogger(__name__)
@@ -10,7 +13,6 @@ logger = logging.getLogger(__name__)
 @router.message(CommandStart())
 async def start_command(message: Message):
     name, id = message.from_user.full_name, message.from_user.id
+    formatting_name = "<b>" + html.escape(name) + "</b>"
     logging.info(f"Пользователь {name}({id}) запустил бота")
-    await message.answer(
-        "Привет! Ты можешь ознакомиться с командами, написав /help"
-        )
+    await message.answer(lexicon["start"].format(formatting_name))
